2b1d7098cae4b3365bd3fadcef918d34e5facf43
Nouf AlShalhoub <Nouf.fawaz3@gmail.com>
2022-03-05 12:27:36 +0300

    Update UserInterfaceState.xcuserstate

--- Yumn/Controller/ContinueViewController.swift
+++ Yumn/Controller/ContinueViewController.swift
@@ -690,7 +690,7 @@ class ContinueViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     func transitionToHome(){
         
         // I have to check if the user is volunteer or hospital, in the log in
-       let volunteerHomeViewController =  storyboard?.instantiateViewController(identifier: Constants.Storyboard.volunteerHomeViewController) as? HomeViewController
+       let volunteerHomeViewController =  storyboard?.instantiateViewController(identifier: Constants.Storyboard.volunteerHomeViewController) as? TabBarController
         
         view.window?.rootViewController = volunteerHomeViewController
         view.window?.makeKeyAndVisible()
