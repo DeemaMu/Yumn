7aa522f3f031300a441d6e8fb5bd81bc292b5313
Rawan Mohammed <53663822+RawanAldosari@users.noreply.github.com>
2022-03-14 10:58:02 +0300

    updates

--- Yumn/Controller/BloodDonationViewController.swift
+++ Yumn/Controller/BloodDonationViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import SwiftUI
-//import BetterSegmentedControl
 import MapKit
 import CoreLocation
 import Firebase
+import SnapKit
+import MaterialDesignWidgets
+
 
 class BloodDonationViewController: UIViewController, CustomSegmentedControlDelegate {
     
@@ -24,6 +26,8 @@ class BloodDonationViewController: UIViewController, CustomSegmentedControlDeleg
     var sortedHospitals:[Location]?
     var hController = HospitalsController()
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     let db = Firestore.firestore()
     
     override func viewDidLoad() {
@@ -31,22 +35,17 @@ class BloodDonationViewController: UIViewController, CustomSegmentedControlDeleg
         
         tableMain.isHidden = true
         
-        //        print("\(String(describing: userLocation))")
-        //        let control = BetterSegmentedControl(frame: CGRect(x: 0, y: 0,width: seg2.frame.width,        height: 50.0))
-        //        let control = BetterSegmentedControl.init(frame: CGRect(x: 0, y: 0, width:                    seg2.frame.width, height: 50), segments: , index: , options: )
-        
-        //        segmentedControl.selectedSegmentIndex = 2
-        //        segmentedControl.removeBorder()
-        //        segmentedControl.addUnderlineForSelectedSegment()
-        //
         
         codeSegmented = CustomSegmentedControl(frame: CGRect(x: 0, y: 0, width: segmentsView.frame.width, height: 50), buttonTitle: ["مراكز التبرع","الإرشادات","الإحتياج"])
         codeSegmented!.backgroundColor = .clear
-        //        codeSegmented.delegate?.change(to: 2)
-        segmentsView.addSubview(codeSegmented!)
+//        segmentsView.addSubview(codeSegmented!)
         
         
         codeSegmented?.delegate = self
+        
+        segmentedControl.selectedSegmentIndex = 2
+        segmentedControl.removeBorder()
+        segmentedControl.addUnderlineForSelectedSegment()
         
         sortedHospitals = getHospitals()
         
@@ -59,6 +58,7 @@ class BloodDonationViewController: UIViewController, CustomSegmentedControlDeleg
         self.navigationController?.navigationBar.layoutIfNeeded()
         self.tabBarController?.tabBar.backgroundColor = UIColor.white
         
+        addSegments()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -71,6 +71,29 @@ class BloodDonationViewController: UIViewController, CustomSegmentedControlDeleg
     override func viewWillDisappear(_ animated: Bool) {
     }
     
+    func addSegments(){
+        
+        let segments = ["مراكز التبرع", "الإحتياج", "إرشادات"]
+        
+        let sgLine = MaterialSegmentedControl(selectorStyle: .line, fgColor: .gray, selectedFgColor: UIColor.init(named: "mainLight")!, selectorColor: UIColor.init(named: "mainLight")!, bgColor: .white)
+        
+        guard let customFont = UIFont(name: "Tajawal", size: 15) else {
+            fatalError("""
+                             Failed to load the "Tajawal" font.
+                             Make sure the font file is included in the project and the font name is spelled correctly.
+                             """
+            )
+        }
+        
+        for i in 0..<3 {
+            sgLine.appendTextSegment(text: segments[i], textColor: .gray, font: customFont, rippleColor: UIColor.init(named: "mainLight")!, cornerRadius: CGFloat(0))
+        }
+        
+        sgLine.frame = CGRect(x: 2, y: 2, width: segmentsView.frame.width - 4 , height: segmentsView.frame.height - 4)
+                
+        segmentsView.addSubview(sgLine)
+        segmentsView.semanticContentAttribute = .forceRightToLeft
+    }
     
     func change(to index: Int) {
         print("segmentedControl index changed to \(index)")
@@ -104,6 +127,101 @@ extension BloodDonationViewController: UITableViewDataSource {
     }
     
     
+}
+
+
+extension BloodDonationViewController{
+   
+        
+        @IBAction func segmentedControlDidChange(_ sender: UISegmentedControl){
+            segmentedControl.changeUnderlinePosition()
+            print("index changed")
+        }
+        
+        override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            segmentedControl.setupSegment()
+        }
+        
+        
+    }
+
+    extension UISegmentedControl {
+        
+        func removeBorder(){
+            
+            guard let customFont = UIFont(name: "Tajawal", size: 18) else {
+                fatalError("""
+                                 Failed to load the "Tajawal" font.
+                                 Make sure the font file is included in the project and the font name is spelled correctly.
+                                 """
+                )
+            }
+            
+            self.tintColor = UIColor.clear
+            self.backgroundColor = UIColor.clear
+            self.setTitleTextAttributes( [NSAttributedString.Key.foregroundColor : UIColor.init(named: "mainLight")!, NSAttributedString.Key.font: customFont, NSAttributedString.Key.underlineColor: UIColor.init(named: "mainLight")!, NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue], for: .selected)
+            self.setTitleTextAttributes( [NSAttributedString.Key.foregroundColor : UIColor.gray,  NSAttributedString.Key.font: customFont], for: .normal)
+            if #available(iOS 13.0, *) {
+                self.selectedSegmentTintColor = UIColor.clear
+            }
+            
+            
+            setBackgroundImage(imageWithColor(color: backgroundColor ?? UIColor.white), for: .normal, barMetrics: .default)
+            setBackgroundImage(imageWithColor(color: UIColor.white), for: .selected, barMetrics: .default)
+            tintColor = UIColor.init(named: "mainLight")
+            
+            setDividerImage(imageWithColor(color: UIColor.clear), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+            //       setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.init(named: "mainLight")!, NSAttributedString.Key.font : customFont, NSAttributedString.Key.underlineColor: UIColor.init(named: "mainLight")!, NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue], for: .selected)
+        }
+        
+        
+        private func imageWithColor(color: UIColor) -> UIImage {
+            let rect = CGRect(x: 0.0, y: 0.0, width:  1.0, height: 1.0)
+            UIGraphicsBeginImageContext(rect.size)
+            let context = UIGraphicsGetCurrentContext()
+            context!.setFillColor(color.cgColor);
+            context!.fill(rect);
+            let image = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            return image!
+        }
+        
+        func setupSegment() {
+    //        self.removeBorder()
+    //        let segmentUnderlineWidth: CGFloat = self.bounds.width
+    //        let segmentUnderlineHeight: CGFloat = 4.0
+    //        let segmentUnderlineXPosition = self.bounds.minX
+    //        let segmentUnderLineYPosition = self.bounds.size.height - 1.0
+    //        let segmentUnderlineFrame = CGRect(x: segmentUnderlineXPosition, y: segmentUnderLineYPosition, width: segmentUnderlineWidth, height: segmentUnderlineHeight)
+    //        let segmentUnderline = UIView(frame: segmentUnderlineFrame)
+    //        segmentUnderline.backgroundColor = UIColor.clear
+            self.removeBorder()
+    //        self.addSubview(segmentUnderline)
+            self.addUnderlineForSelectedSegment()
+        }
+        
+        func addUnderlineForSelectedSegment(){
+            
+            let underlineWidth: CGFloat = self.bounds.size.width / CGFloat(self.numberOfSegments)
+            let underlineHeight: CGFloat = 4.0
+            let underlineXPosition = CGFloat(selectedSegmentIndex * Int(underlineWidth))
+            let underLineYPosition = self.bounds.size.height - 1.0
+            let underlineFrame = CGRect(x: underlineXPosition, y: underLineYPosition, width: underlineWidth, height: underlineHeight)
+            let underline = UIView(frame: underlineFrame)
+            underline.backgroundColor = UIColor.init(named: "mainLight")
+            underline.tag = 1
+            self.addSubview(underline)
+            
+            
+        }
+        
+        func changeUnderlinePosition(){
+            guard let underline = self.viewWithTag(1) else {return}
+            let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
+            underline.frame.origin.x = underlineFinalXPosition
+            
+        }
+
 }
 
 //
