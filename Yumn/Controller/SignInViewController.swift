f1791bd644d74c2696f5a069372f60038c40a546
Deema AlMutairi <dema77.da@gmail.com>
2022-03-15 10:46:47 +0300

    Revert "Merge branch 'Deema' into Stage"
    This reverts commit d8855e506c53f53ee050d1cef23f79977c1a39d3, reversing
    changes made to 585e2e8381dec0017e210fa7dc4bda035e94abdc.

--- Yumn/Controller/SignInViewController.swift
+++ Yumn/Controller/SignInViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
-    let isUserLoggedIn:Bool = UserDefaults.standard.bool(forKey: "isUserLoggedIn")
    // @IBOutlet weak var pView: UIView!
     
     @IBOutlet var mainView: UIView!
@@ -63,32 +62,6 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        let db = Firestore.firestore()
-        if (isUserLoggedIn){
-            
-            // Check if the user is a volunteer by using the uid
-            let docRef = db.collection("volunteer").document(Auth.auth().currentUser?.uid ?? "")
-            
-            docRef.getDocument { [self] (document, error) in
-                if let document = document, document.exists {
-                    
-                    print("Vol")
-                    DispatchQueue.main.async {
-                    transitionToHome()
-                    }
-                 
-                    
-                } else {
-                    print("Man")
-                    DispatchQueue.main.async {
-                    transitionToHospitalHome()
-                    }
-                }
-                
-            }
-        }
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
@@ -406,9 +379,7 @@ class SignInViewController: UIViewController {
                 
                 // There exists a user and signed in
             {
-                UserDefaults.standard.set(true, forKey: "isUserLoggedIn")
-                UserDefaults.standard.synchronize()
-                
+                    
                     // Check which user
                     let db = Firestore.firestore()
                     
