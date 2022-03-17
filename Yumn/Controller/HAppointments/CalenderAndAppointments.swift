acaf8a1d53cb7dba1902f3bc90da081805c9a2e5
Rawan Mohammed <53663822+RawanAldosari@users.noreply.github.com>
2022-03-17 06:00:05 +0300

    updates

---
+++ Yumn/Controller/HAppointments/CalenderAndAppointments.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderAndAppointments.swift
+//  Yumn
+//
+//  Created by Rawan Mohammed on 17/03/2022.
+//
+
+import SwiftUI
+
+struct CalenderAndAppointments: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalenderAndAppointments_Previews: PreviewProvider {
+    static var previews: some View {
+        CalenderAndAppointments()
+    }
+}
