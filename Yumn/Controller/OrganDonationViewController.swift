52a13e6202de3a0f382cbb94f622d977fbd725a3
Nouf AlShalhoub <Nouf.fawaz3@gmail.com>
2022-03-20 18:35:24 +0300

    organ don info updates

--- Yumn/Controller/OrganDonationViewController.swift
+++ Yumn/Controller/OrganDonationViewController.swift
@@ -14,9 +14,23 @@ import Firebase
 
 class OrganDonationViewController: UIViewController, CustomSegmentedControlDelegate {
     
-    var location:CLLocation?
-    var userLocation:CLLocationCoordinate2D?
-    @IBOutlet weak var seg2: UIView!
+    @IBOutlet weak var organBtn2: UIButton!
+    @IBOutlet weak var arrow2: UIButton!
+    @IBOutlet weak var organBtn1: UIButton!
+    @IBOutlet weak var arrow1: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var organDonImage: UIImageView!
+    @IBOutlet weak var organsStack1: UIStackView!
+    @IBOutlet weak var organsStack2: UIStackView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+   // var location:CLLocation?
+   // var userLocation:CLLocationCoordinate2D?
+ //   @IBOutlet weak var seg2: UIView!
+    
+    
     var codeSegmented:CustomSegmentedControl? = nil
     @IBOutlet weak var segmentsView: UIView!
     
@@ -27,7 +41,24 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
     
     override func viewDidLoad() {
         
-      //  roundView.layer.cornerRadius = 30
+        //Organ Donation Information
+        label1.text = "يمكن التبرع بـ:"
+        label2.text = "- جزء من الكبد"
+        label3.text = "- إحدى الكليتين"
+        arrow1.contentHorizontalAlignment = .left
+        arrow2.contentHorizontalAlignment = .left
+        organBtn1.contentHorizontalAlignment = .right
+        organBtn2.contentHorizontalAlignment = .right
+        organDonImage.superview?.bringSubviewToFront(organDonImage)
+        stackView.superview?.bringSubviewToFront(stackView)
+        organsStack1.clipsToBounds = true
+        organsStack2.clipsToBounds = true
+        organsStack1.isHidden = true
+        organsStack2.isHidden = true
+        organDonImage.isHidden = true
+        stackView.isHidden = true
+        
+
         
         super.viewDidLoad()
         
@@ -42,15 +73,11 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
         //        segmentedControl.addUnderlineForSelectedSegment()
         //
         
-        codeSegmented = CustomSegmentedControl(frame: CGRect(x: 0, y: 0, width: segmentsView.frame.width, height: 50), buttonTitle: ["مراكز التبرع","الإرشادات","الإحتياج"])
+        codeSegmented = CustomSegmentedControl(frame: CGRect(x: 0, y: 0, width: segmentsView.frame.width, height: 50), buttonTitle: ["نوع التبرع","الإرشادات","الإحتياج"])
         codeSegmented!.backgroundColor = .clear
         //        codeSegmented.delegate?.change(to: 2)
         segmentsView.addSubview(codeSegmented!)
-        
-        
         codeSegmented?.delegate = self
-        
-        
         
         
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for:.default)
@@ -62,11 +89,92 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
     func change(to index: Int) {
         print("segmentedControl index changed to \(index)")
         if(index==0){
+            
+            organDonImage.isHidden = true
+            stackView.isHidden = true
+            
+            
         }
-        if(index != 0){
+        else if(index == 1){
+            
+            organDonImage.isHidden = false
+            stackView.isHidden = false
+            
+        }
+        
+        else {
+            
+            organDonImage.isHidden = true
+            stackView.isHidden = true
+            
         }
     }
     
-}
+    
+    
+    
+    //Organ Donation Information
+    @IBAction func onPressedArrow1(_ sender: Any) {
+        
+        organsStack1.isHidden = !organsStack1.isHidden
+       
+       if (organsStack1.isHidden == false){
+        arrow1.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+       }
+       
+       else{
+           arrow1.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+
+       }
+
+    }
+    
+    @IBAction func onPressedOrganBtn1(_ sender: Any) {
+        
+        organsStack1.isHidden = !organsStack1.isHidden
+       
+       if (organsStack1.isHidden == false){
+        arrow1.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+       }
+       
+       else{
+           arrow1.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+
+       }
+    }
+    
+    
+    @IBAction func onPressedOrganBtn2(_ sender: Any) {
+        organsStack2.isHidden = !organsStack2.isHidden
+       
+       if (organsStack2.isHidden == false){
+        arrow2.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+       }
+       
+       else{
+           arrow2.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+
+       }
+    }
+    @IBAction func onPressedArrow2(_ sender: Any) {
+        
+        organsStack2.isHidden = !organsStack2.isHidden
+       
+       if (organsStack2.isHidden == false){
+        arrow2.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+       }
+       
+       else{
+           arrow2.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+
+       }
+    }
+    }
+
+    
+
+    
+    
+
 
 
