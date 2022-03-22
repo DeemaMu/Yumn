58a5c54e4f33fee7fddfe1a74a7e26f2d6a19b60
Modhi1 <modhi2578@gmail.com>
2022-03-22 06:22:36 +0300

    added organ chart
    added organ chart

--- Yumn/Controller/VOrganDonation/OrganDonationViewController.swift
+++ Yumn/Controller/VOrganDonation/OrganDonationViewController.swift
@@ -11,8 +11,9 @@ import SwiftUI
 import MapKit
 import CoreLocation
 import Firebase
-
-class OrganDonationViewController: UIViewController, CustomSegmentedControlDelegate {
+import FirebaseFirestore
+import Charts
+class OrganDonationViewController: UIViewController, CustomSegmentedControlDelegate, ChartViewDelegate{
     
     @IBOutlet weak var organBtn2: UIButton!
     @IBOutlet weak var arrow2: UIButton!
@@ -29,7 +30,13 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
    // var location:CLLocation?
    // var userLocation:CLLocationCoordinate2D?
  //   @IBOutlet weak var seg2: UIView!
-    
+    // by Modhi
+    @IBOutlet weak var loadingGif: UIImageView!
+    @IBOutlet weak var blurredView: UIView!
+    @IBOutlet weak var organPieWholeContainer: UIView!
+    @IBOutlet weak var cityOfUser: UILabel!
+    @IBOutlet weak var viewOrganPie: UIView!
+    var pieChart = PieChartView()
     
     var codeSegmented:CustomSegmentedControl? = nil
     @IBOutlet weak var segmentsView: UIView!
@@ -59,6 +66,19 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
         stackView.isHidden = true
         
 
+        
+        
+        // by Modhi
+        pieChart.delegate = self
+        
+        organPieWholeContainer.isHidden = false
+        viewOrganPie.isHidden = false
+        cityOfUser.isHidden = false
+        
+        loadingGif.superview?.bringSubviewToFront(loadingGif)
+        loadingGif.loadGif(name: "yumnLoading")
+        loadingGif.isHidden = false
+        blurredView.isHidden = false
         
         super.viewDidLoad()
         
@@ -93,6 +113,13 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
             organDonImage.isHidden = true
             stackView.isHidden = true
             
+            // by Modhi
+            organPieWholeContainer.isHidden = true
+            viewOrganPie.isHidden = true
+            cityOfUser.isHidden = true
+            loadingGif.isHidden = true
+            blurredView.isHidden = true
+            
             
         }
         else if(index == 1){
@@ -100,12 +127,26 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
             organDonImage.isHidden = false
             stackView.isHidden = false
             
+            // by Modhi
+            organPieWholeContainer.isHidden = true
+            viewOrganPie.isHidden = true
+            cityOfUser.isHidden = true
+            loadingGif.isHidden = true
+            blurredView.isHidden = true
+            
         }
         
         else {
             
             organDonImage.isHidden = true
             stackView.isHidden = true
+            
+            // by Modhi
+            organPieWholeContainer.isHidden = false
+            viewOrganPie.isHidden = false
+            cityOfUser.isHidden = false
+            loadingGif.isHidden = false
+            blurredView.isHidden = false
             
         }
     }
@@ -173,7 +214,169 @@ class OrganDonationViewController: UIViewController, CustomSegmentedControlDeleg
 
        }
     }
+    
+    
+    
+    // by modhi
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+
+        guard let customFont2 = UIFont(name: "Tajawal-Regular", size: UIFont.labelFontSize) else {
+            fatalError("""
+                Failed to load the "CustomFont-Light" font.
+                Make sure the font file is included in the project and the font name is spelled correctly.
+                """
+            )
+        }
+        
+
+        
+        // updated
+        cityOfUser.font = UIFontMetrics.default.scaledFont(for: customFont2)
+        cityOfUser.adjustsFontForContentSizeCategory = true
+        cityOfUser.font = cityOfUser.font.withSize(24)
+
+        
+        // for rounded top corners (view)
+        if #available(iOS 11.0, *) {
+                self.viewOrganPie.clipsToBounds = true
+            viewOrganPie.layer.cornerRadius = 35
+            viewOrganPie.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            } else {
+                // Fallback on earlier versions
+            }
+        
+        
+        // show loading indicator when data is being fetched
+      //  loadingIndicator()
+        
+        setUpChart()
+        
+        getTotalOrganShortage(completion: { totalOrganShortge in
+            if let TOS = totalOrganShortge {
+                
+                self.loadingGif.isHidden = true
+                self.blurredView.isHidden = true
+                
+                self.populateChart(TOS: TOS)
+                
+            } else {
+                //handle nil response
+                print("couldn't build pie chart")
+            }
+        })
+        
+       
+        
     }
+    
+    
+    func getTotalOrganShortage( completion: @escaping (totalOrganShortage?)->())  {
+   
+        // change collection
+        cityOfUser.text = "رسم بياني لاحتياج الأعضاء في المملكة"
+        let docRef = db.collection("hospital").document(Constants.FStore.totalOrganShDoc)
+
+        // Get data
+               docRef.getDocument { (document, error) in
+                   guard let document = document, document.exists else {
+                       print("Document does not exist, there was an issue fetching total organs shortage from firestore. ")
+                       return
+                   }
+                   let dataDescription = document.data()
+                let totalOrganShort = totalOrganShortage(
+                    heart: dataDescription![Constants.FStore.heart] as! Int,
+                    lung: dataDescription![Constants.FStore.lung] as! Int,
+                    kidney: dataDescription![Constants.FStore.kidney] as! Int,
+                    liver: dataDescription![Constants.FStore.liver] as! Int,
+                    pancreas: dataDescription![Constants.FStore.pancreas] as! Int,
+                    intestine: dataDescription![Constants.FStore.intestine] as! Int,
+                    cornea: dataDescription![Constants.FStore.cornea] as! Int,
+                    boneMarrow: dataDescription![Constants.FStore.boneMarrow] as! Int)
+                
+                
+                completion(totalOrganShort)
+               }
+    }
+    
+    func setUpChart(){
+        
+        pieChart.frame = CGRect(x: 0, y: 0,
+                                width: self.organPieWholeContainer.frame.size.width,
+                                height: self.organPieWholeContainer.frame.size.width)
+        //pieChart.center = organPieWholeContainer.center
+        viewOrganPie.addSubview(pieChart)
+        
+    }
+    
+    
+    
+    
+    func populateChart(TOS :  totalOrganShortage){
+        
+        
+        
+        let totalOSh : Int = TOS.heart + TOS.lung +
+            TOS.liver + TOS.kidney +
+            TOS.pancreas + TOS.cornea +
+            TOS.intestine + TOS.boneMarrow
+        
+                        
+        let heartPercentage : Int = TOS.heart
+        let lungPercentage : Int = TOS.lung
+        let kidneyPercentage : Int = TOS.kidney
+        let liverPercentage : Int = TOS.liver
+        
+        let pancreasPercentage : Int = TOS.pancreas
+        let intestinePercentage : Int = TOS.intestine
+        let boneMarrowPercentage : Int = TOS.boneMarrow
+        let corneaPercentage : Int = TOS.cornea
+        
+       
+        
+        let pieChartEntry: [PieChartDataEntry] = [
+            
+        
+            PieChartDataEntry(value: Double((Double(heartPercentage)/Double(totalOSh))*100), label: "قلب"),
+            PieChartDataEntry(value: Double((Double(lungPercentage)/Double(totalOSh))*100), label: "رئة"),
+            PieChartDataEntry(value: Double((Double(kidneyPercentage)/Double(totalOSh))*100), label: "كلية"),
+            PieChartDataEntry(value: Double((Double(liverPercentage)/Double(totalOSh))*100), label: "كبد"),
+            
+            
+            PieChartDataEntry(value: Double((Double(pancreasPercentage)/Double(totalOSh))*100), label: "بنكرياس"),
+            PieChartDataEntry(value: Double((Double(intestinePercentage)/Double(totalOSh))*100), label: "امعاء"),
+            PieChartDataEntry(value: Double((Double(boneMarrowPercentage)/Double(totalOSh))*100), label: "نخاع العظم"),
+            PieChartDataEntry(value: Double((Double(corneaPercentage)/Double(totalOSh))*100), label: "القرنية")
+        ]
+        
+        let set = PieChartDataSet(entries: pieChartEntry)
+        //set.colors = ChartColorTemplates.colorful()
+        set.colors = [Constants.Colors.green, Constants.Colors.yellow, Constants.Colors.orange, Constants.Colors.pink, Constants.Colors.green, Constants.Colors.yellow, Constants.Colors.orange, Constants.Colors.pink]
+        let data = PieChartData(dataSet: set)
+        
+        let pFormater = NumberFormatter()
+        pFormater.numberStyle = .percent
+        pFormater.maximumFractionDigits = 1
+        pFormater.multiplier = 1
+        pFormater.percentSymbol = "%"
+        
+        data.setValueFormatter(DefaultValueFormatter(formatter: pFormater))
+        self.pieChart.data = data
+        //self.pieChart.usePercentValueEnabled = true
+       
+        self.pieChart.legend.enabled=false
+        /* let legend = self.pieChart.legend
+         legend.horizontalAlignment = .center
+         legend.verticalAlignment = .bottom
+         legend.orientation = .horizontal*/
+         // suppose to end here
+       //  self.pieChart.data=data
+    }
+    
+    } // end class
 
     
 
