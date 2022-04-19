0314ddd2754a49ddf93cbeba4e2823aa24de15ac
Rawan Mohammed <53663822+RawanAldosari@users.noreply.github.com>
2022-04-19 15:35:20 +0300

    Update AfterDeathOrganSelection.swift

--- Yumn/Controller/VOrganDonation/AfterDeath/AfterDeathOrganSelection.swift
+++ Yumn/Controller/VOrganDonation/AfterDeath/AfterDeathOrganSelection.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import UIKit
 
 
 struct AfterDeathOrganSelection: View {
@@ -21,35 +22,60 @@ struct AfterDeathOrganSelection: View {
         GridItem(.flexible(), spacing: 0, alignment: nil),
     ]
     
+    @ObservedObject var organsVM = OrgansVM()
+
     var organsPointer = 0
     
     let organs: [String] = ["الكلى", "الكبد", "الرئتين", "البنكرياس", "القلب", "الامعاء", "العظام", "نخاع العظم", "الجلد", "القرنيات"]
-    let selected = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    var selected = [
+        "الكلى": false,
+        "الكبد":false,
+        "الرئتين":false,
+        "البنكرياس":false,
+        "القلب":false,
+        "الامعاء":false,
+        "العظام":false,
+        "نخاع العظم":false,
+        "الجلد":false,
+        "القرنيات":false,
+    ]
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false){
-            LazyVGrid(columns: items, alignment: .center) {
-                ForEach(organs.dropLast(organs.count % 3), id: \.self) { index in
-                    card(organ: index)
-                }
-            }
-            LazyHStack {
-                ForEach(organs.suffix(organs.count % 3), id: \.self) { index in
-                    card(organ: index as! String)
-                }
-            }
+        VStack(spacing: 20){
+            HStack(){
+                
+                Spacer()
+                Text("الأعضاء المراد التبرع بها:").font(Font.custom("Tajawal", size: 15))
+                                .foregroundColor(mainDark)
+            }.padding(.trailing)
+            
+            
+            ScrollView(.vertical, showsIndicators: false){
+                        LazyVGrid(columns: items, alignment: .center) {
+                            ForEach(organs.dropLast(organs.count % 3), id: \.self) { index in
+                                card(organ: index)
+                            }
+                        }
+                        LazyHStack {
+                            ForEach(organs.suffix(organs.count % 3), id: \.self) { index in
+                                card(organ: index as! String)
+                            }
+                        }
+                    }
         }
+        
     }
     
     
     @ViewBuilder
     func card(organ: String) -> some View{
+        
         Button(action: {
-            buttonPressed()
+            buttonPressed(organ: organ)
         }
         ) {
             Text(organ).font(Font.custom("Tajawal", size: 15))
-                .foregroundColor(.white)
+                .foregroundColor(self.organsVM.selected[organ]! ? .white : mainDark)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
@@ -57,16 +83,36 @@ struct AfterDeathOrganSelection: View {
                 cornerRadius: 10,
                 style: .continuous
             )
-                .fill(mainDark)
+                .fill(self.organsVM.selected[organ]! ? mainDark : .white)
         )
         .frame(width: 90, height: 90, alignment: .center)
+        .shadow(color: Color(#colorLiteral(red: 0.8653315902, green: 0.8654771447, blue: 0.8653123975, alpha: 1)), radius: 3, x: 0, y: 6)
     }
     
-    func buttonPressed(){
-        
+//    #colorLiteral(red: 0.8653315902, green: 0.8654771447, blue: 0.8653123975, alpha: 1)
+    
+    func buttonPressed(organ: String){
+        self.organsVM.selected[organ]?.toggle()
     }
     
 }
+
+class OrgansVM: ObservableObject {
+    @Published var selected = [
+        "الكلى": false,
+        "الكبد":false,
+        "الرئتين":false,
+        "البنكرياس":false,
+        "القلب":false,
+        "الامعاء":false,
+        "العظام":false,
+        "نخاع العظم":false,
+        "الجلد":false,
+        "القرنيات":false,
+    ]
+}
+
+
 
 struct AfterDeathOrganSelection_Previews: PreviewProvider {
     static var previews: some View {
