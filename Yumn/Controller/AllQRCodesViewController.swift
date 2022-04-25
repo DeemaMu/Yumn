3b7dd28762f9a398ff1cfa83de4747fa5c23dec4
Nouf AlShalhoub <Nouf.fawaz3@gmail.com>
2022-04-26 00:01:35 +0300

    QR code cell

--- Yumn/Controller/AllQRCodesViewController.swift
+++ Yumn/Controller/AllQRCodesViewController.swift
@@ -7,9 +7,13 @@
 
 import UIKit
 import Firebase
+import Foundation
 
 
 class AllQRCodesViewController: UIViewController, CustomSegmentedControlDelegate {
+    
+    var sortedValidQRCodes:[QRCode]?
+
 
     @IBOutlet weak var validQRCodesTableView: UITableView!
     
@@ -57,6 +61,9 @@ class AllQRCodesViewController: UIViewController, CustomSegmentedControlDelegate
                  
                  
              }
+        
+        
+        
     /*
     // MARK: - Navigation
 
@@ -69,3 +76,117 @@ class AllQRCodesViewController: UIViewController, CustomSegmentedControlDelegate
 
 }
 }
+
+
+extension AllQRCodesViewController{
+    
+
+    
+    func getValidQRCodes() -> [QRCode]{
+        
+        var validQRCodes:[QRCode] = []
+       
+        let db = Firestore.firestore()
+
+        
+        db.collection("code").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print ("Error getting the documents:  \(err)")
+            } else {
+                for document in
+                        querySnapshot!.documents{
+                    let doc = document.data()
+                    
+                    let dateCreated: String = doc["dateCreated"] as! String
+                    
+                    let amount: Double =  doc["dateCreated"] as! Double
+                    
+                    let id: String = doc["id"] as! String
+                    
+                    let status: String = doc["status"] as! String
+                    
+                    validQRCodes.append(QRCode(amount: amount, dateCreated: dateCreated, id: id, status: status ))
+                }
+                    self.sortedValidQRCodes = validQRCodes.sorted(by: { (QR0: QRCode, QR1: QRCode) -> Bool in
+                        return QR0.amount < QR1.amount
+                    })
+                    
+                    DispatchQueue.main.async {
+                        self.validQRCodesTableView.reloadData()
+                    }
+                
+
+                    
+                    
+                    
+                    
+                    
+                }
+            }
+            
+            let validQRCodes2 = validQRCodes.sorted(by: { (QR0: QRCode, QR1: QRCode) -> Bool in
+                return QR0.amount < QR1.amount
+            })
+            
+            return validQRCodes2
+            
+        
+
+            
+        }
+    
+    
+    
+    func  convertEngToArabic(num: Double)-> String{
+        
+        let points=String(num)
+        var arabicString=""
+        
+        for ch in points{
+            
+            switch ch {
+                
+            case "0":
+                arabicString+="٠"
+            case "9":
+                arabicString+="٩"
+            case "8":
+                arabicString+="٨"
+            case "7":
+                arabicString+="٧"
+            case "6":
+                arabicString+="٦"
+            case "5":
+                arabicString+="٥"
+            case "4":
+                arabicString+="٤"
+            case "3":
+                arabicString+="٣"
+            case "2":
+                arabicString+="٢"
+            case "1":
+                arabicString+="١"
+            case ".":
+                arabicString+="."
+                
+            default:
+                arabicString="٠"
+            }
+        }
+        return arabicString
+    }
+
+
+    
+    
+
+    
+    
+    
+    
+    
+}
+    
+    
+
+
