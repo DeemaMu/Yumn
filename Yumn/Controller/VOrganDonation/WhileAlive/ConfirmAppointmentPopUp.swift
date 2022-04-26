ca155ef61b86931569911f58d3f557eecd865174
Rawan Mohammed <53663822+RawanAldosari@users.noreply.github.com>
2022-04-26 23:34:02 +0300

    updates

---
+++ Yumn/Controller/VOrganDonation/WhileAlive/ConfirmAppointmentPopUp.swift
@@ -0,0 +1,20 @@
+//
+//  ConfirmAppointmentPopUp.swift
+//  Yumn
+//
+//  Created by Rawan Mohammed on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ConfirmAppointmentPopUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfirmAppointmentPopUp_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmAppointmentPopUp()
+    }
+}
